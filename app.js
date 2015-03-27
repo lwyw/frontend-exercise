0eef7430d1fc660ca6823b165be87b3b62b82937
weeyoung <weeyoungl@gmail.com>
2015-03-28 02:26:47 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-/*
-    # Endpoint URL #
+'use strict';
 
-    https://api.github.com/legacy/repos/search/{query}
+var app = angular.module("tradeGeckoApp", []);
 
-    Note: Github imposes a rate limit of 60 request per minute. Documentation can be found at http://developer.github.com/v3/.
+app.controller("tradeGeckoCtrl", ["$scope", "$http", function ($scope, $http) {
+  function resetButton() {
+    $scope.searchButtonLabel = "search";
+    $scope.disableSearchButton = false;
+  }
 
-    # Example Response JSON #
+  $scope.searchGitHub = function () {
+    //change search button label
+    $scope.searchButtonLabel = "searching...";
+    $scope.disableSearchButton = true;
 
-    {
-      "repositories": [
-        {
-          "created_at": "2014-01-13T02:37:26Z",
-          "description": "A Ruby interface to the TradeGecko API.",
-          "followers": 1,
-          "fork": false,
-          "forks": 2,
-          "has_downloads": true,
-          "has_issues": true,
-          "has_wiki": true,
-          "homepage": null,
-          "language": "Ruby",
-          "name": "gecko",
-          "open_issues": 3,
-          "owner": "tradegecko",
-          "private": false,
-          "pushed": "2014-07-29T08:18:51Z",
-          "pushed_at": "2014-07-29T08:18:51Z",
-          "score": 16.24838,
-          "size": 1617,
-          "type": "repo",
-          "url": "https://github.com/tradegecko/gecko",
-          "username": "tradegecko",
-          "watchers": 1
-        }
-      ]
-    }
-*/
+    //github api request
+    $http.get("https://api.github.com/legacy/repos/search/" + encodeURIComponent($scope.keyword))
+      .success(function (data) {
+        //populate repositories scope
+        $scope.repos = data.repositories;
+        //reset button label and state
+        resetButton();
+      })
+      .error(function (err) {
+        //show error in console log
+        console.log(err);
+        //reset button label and state
+        resetButton();
+      });
+  };
 
+  $scope.showRepo = function (index) {
+    $scope.repos[index].show = !$scope.repos[index].show;
+  };
 
+  //initialize
+  $scope.header = "TradeGecko";
+  resetButton();
+}]);
